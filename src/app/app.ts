c621df85c17342416b9a9472a9df143c4144f94b
iqbaladitama91 <iqbaladitama91@gmail.com>
2026-01-11 12:34:04 +0700

    feat: add to do list ui

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('to-do-list-frontend');
+
+  protected toDoList = signal([]);
+  
+  protected darkMode = signal(false);
+
+  toggleTheme() {
+    this.darkMode.update(isDark => !isDark);
+    if (this.darkMode()) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }
 }
