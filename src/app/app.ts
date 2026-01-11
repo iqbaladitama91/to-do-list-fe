5d77652a6d045d2fd427631417ed36f6f9acc9c7
Iqbal Aditama <iqbaladitama91@gmail.com>
2026-01-11 15:17:50 +0700

    feat: add to do list add method

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,56 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+interface ToDoItem {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('to-do-list-frontend');
 
-  protected toDoList = signal([]);
-  
+  protected toDoList = signal<ToDoItem[]>([]);
+
+  protected showModal = signal(false)
+
   protected darkMode = signal(false);
+
+  protected newTodoList = new FormControl('');
+
+  handleSubmitData() {
+    const newItem: ToDoItem = {
+      id: this.toDoList().length + 1,
+      title: this.newTodoList.value || 'Untitled Task',
+      completed: false
+    };
+
+    this.toDoList.update(list => [...list, newItem]);
+    this.newTodoList.setValue('');
+    this.showModal.set(false);
+  }
+
+
+  handleCheckboxChange(id: number) {
+    this.toDoList.update(list =>
+      list.map(item =>
+        item.id === id ? { ...item, completed: !item.completed } : item
+      )
+    );
+  }
+
+  handleShowModal(isOpen: boolean) {
+    this.showModal.set(isOpen);
+  }
+
+
 
   toggleTheme() {
     this.darkMode.update(isDark => !isDark);
